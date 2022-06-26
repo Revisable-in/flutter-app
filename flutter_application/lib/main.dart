227d7f2106868588e8ec2ceb42994b51f955ef74
Rudransh Arora <rudransh1482@gmail.com>
2022-06-26 18:13:03 +0530

    linked signup page

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,39 +7,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Revisable',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Revisable'),
+      routes: <String, WidgetBuilder>{'/Signup': (context) => Signup()},
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,18 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        // appBar: AppBar(
-        //   // Here we take the value from the MyHomePage object that was created by
-        //   // the App.build method, and use it to set our appbar title.
-        //   title: Text(widget.title),
-        // ),
         body: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
@@ -71,17 +45,132 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                new AspectRatio(
-                    aspectRatio: 4 / 3,
-                    child: new Image(
-                        image: AssetImage("assets/images/revisable.png"))),
+                new Listener(
+                  onPointerDown: (PointerDownEvent event) {
+                    Navigator.pushNamed(context, '/Signup');
+                  },
+                  child: AspectRatio(
+                      aspectRatio: 1 / 1,
+                      child: new Image(
+                          image: AssetImage("assets/images/revisable.png"))),
+                ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    "REVISABLE",
+                    style: TextStyle(
+                      fontFamily: "Raleway",
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
               ],
-            ))
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: _incrementCounter,
-        //   tooltip: 'Increment',
-        //   child: Icon(Icons.add),
-        // ), // This trailing comma makes auto-formatting nicer for build methods.
-        );
+            )));
   }
+}
+
+class Signup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(fit: StackFit.expand, children: [
+      buildBackground(),
+      Positioned(
+        top: 450.0,
+        left: 40.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "COURSE BASED FLASHCARDS",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "Spaced repetition to help you retain for longer.",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "INSIGHTS & FLASHCARDS",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "Insights which help you study smarter and more consistently.",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "VERSATILE REVISION TOOL",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                "A tool which blends with your study patterns and builds consistency.",
+                style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]));
+  }
+}
+
+Widget buildBackground() {
+  return Scaffold(
+    body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/bgsignup.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  );
 }
